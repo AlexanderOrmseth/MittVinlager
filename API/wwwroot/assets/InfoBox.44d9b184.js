63413eabaaf8cd50dc5fb49632043d18f638bdfb
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-08 01:44:20 +0200

    update build

--- API/wwwroot/assets/InfoBox.44d9b184.js
+++
@@ -1,3 +0,0 @@
-import{R as e}from"./index.3caeb1f1.js";import{I as m}from"./Info.esm.646d4866.js";const o=({message:t,className:r})=>e.createElement("div",{className:`my-4 flex items-center text-sm md:p-2.5 p-2 border-blue-wine-200 
-        dark:border-blue-wine-400 dark:bg-blue-wine-700 dark:text-blue-wine-25 border rounded bg-blue-wine-25
-        font-medium text-blue-wine-500 ${r||""} `},e.createElement(m,{size:"1.5rem",className:"mr-2"}),e.createElement("em",{className:"flex-1"},t));export{o as I};
