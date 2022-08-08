3ef767c10dcaf5c4ea40a0a9ab12e90058693b51
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-08 14:59:24 +0200

    cleared HomePage.tsx

---
+++ API/wwwroot/assets/Spinner.34af1c17.js
@@ -0,0 +1,1 @@
+import{R as e,q as a}from"./index.b63335f9.js";const m=({text:t})=>e.createElement("div",{className:"p-2"},e.createElement(a,{className:"mx-auto",height:"3rem",width:"3rem",stroke:"rgb(55 65 81)"}),e.createElement("p",{className:"mt-2 text-center font-bold text-gray-900 dark:text-gray-300"},t));export{m as S};
