836dadb3188bc2ccd1e510d8eed6ceda2484f384
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-11 00:13:40 +0200

    update build

---
+++ API/wwwroot/assets/useFetchSingleWine.14778d18.js
@@ -0,0 +1,1 @@
+import{ar as i,as as r}from"./index.cba0ca29.js";const u=()=>{const{id:e}=i(),s=e?+e:void 0,t=s?isNaN(s):!0,{data:a,...n}=r(s,{skip:t});return{id:s,wine:a,status:n}};export{u};
--- API/wwwroot/assets/useFetchSingleWine.748c222c.js
+++
@@ -1,1 +0,0 @@
-import{ar as i,as as r}from"./index.8606ae31.js";const u=()=>{const{id:e}=i(),s=e?+e:void 0,t=s?isNaN(s):!0,{data:a,...n}=r(s,{skip:t});return{id:s,wine:a,status:n}};export{u};
