c2dcbc792ece17905a449af39031cf3eefac6900
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-10 02:29:13 +0200

    adjusted toggle filter button size

--- client/src/features/wine/list/WineFilter.tsx
+++ client/src/features/wine/list/WineFilter.tsx
@@ -31,35 +31,7 @@ const WineFilter = ({ metaData, isFetchingWine }: Props) => {
   const isSmall = useMediaQuery("(max-width: 767px)");
   const params = useAppSelector(getParams);
   const { hideFilter } = useAppSelector((state) => state.wine);
-
-  /*const variants = isSmall
-    ? {
-        show: {
-          opacity: 1,
-          scale: 1,
-          height: "auto",
-        },
-        hidden: {
-          opacity: 1,
-          scale: 1,
-          height: 0,
-        },
-      }
-    : {
-        show: {
-          opacity: 1,
-          scale: 1,
-          width: "auto",
-        },
-        hidden: {
-          opacity: 1,
-          scale: 1,
-          width: 0,
-        },
-      };*/
-
   const dispatch = useAppDispatch();
-
   const { data: filters, ...filterStatus } = useGetWineFiltersQuery();
 
   // check for mismatch and reset params
@@ -93,9 +65,9 @@ const WineFilter = ({ metaData, isFetchingWine }: Props) => {
     <>
       <button
         onClick={() => dispatch(setHideFilter(!hideFilter))}
-        className="btn-white i-flex-row justify-center md:hidden"
+        className="btn-white py-1.5 i-flex-row justify-center md:hidden"
       >
-        <Funnel size="1.5rem" />
+        <Funnel size="1.2rem" />
         {hideFilter ? "Vis filter" : "Skjul filter"}
       </button>
 
