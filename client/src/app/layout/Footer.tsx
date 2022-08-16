dbc003166fb119786c43288d4895ffb8c9359c86
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-16 23:50:09 +0200

    refactoring, reduced padding

---
+++ client/src/app/layout/Footer.tsx
@@ -0,0 +1,40 @@
+import { GithubLogo, LinkedinLogo } from "phosphor-react";
+
+const Footer = () => {
+  return (
+    <footer className="relative mx-auto w-full min-w-[320px] max-w-screen-xl border-t py-12 px-2.5 dark:border-gray-700 sm:px-4 md:py-8 xl:px-0">
+      <div className="flex flex-col items-center justify-around gap-x-4 gap-y-6 md:flex-row">
+        <div className="xts:text-sm flex flex-wrap gap-4 text-xs">
+          <a
+            href="https://github.com/AlexanderOrmseth/MittVinlager"
+            target="_blank"
+            className="flex items-center gap-x-1 rounded border-white bg-gray-900 px-2 py-1 text-white hover:bg-black dark:bg-black/60 dark:hover:bg-black"
+            rel="noreferrer"
+          >
+            <GithubLogo size="1.5em" />
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alexander-ormseth-731b11239/"
+            target="_blank"
+            className="flex items-center gap-x-1 rounded border-white bg-[#0a66c2] px-2 py-1  text-white hover:bg-[#004182]"
+            rel="noreferrer"
+          >
+            <LinkedinLogo size="1.5em" />
+            LinkedIn
+          </a>
+        </div>
+        <p className="text-sm text-gray-700 dark:text-gray-300">
+          Utviklet av Alexander Ormseth
+        </p>
+      </div>
+      <p className="absolute left-0 -top-2.5 w-full text-center text-sm text-gray-500 dark:text-gray-400">
+        <span className="bg-white px-2 dark:bg-gray-900">
+          Mitt Vinlager © 2021-{new Date().getFullYear()}
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/app/layout/Layout.tsx
+++ client/src/app/layout/Layout.tsx
@@ -1,48 +1,17 @@
-import { GithubLogo, LinkedinLogo } from "phosphor-react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <>
-      <header className="w-full min-w-[320px] border-b bg-white p-4 dark:border-slate-800 dark:bg-black/10 xl:px-0">
+      <header className="w-full min-w-[320px] border-b bg-white px-2.5 py-4 dark:border-slate-800 dark:bg-black/10 sm:px-4 xl:px-0">
         <Header />
       </header>
-      <main className="mx-auto flex w-full min-w-[320px] max-w-screen-xl grow flex-col px-4 pt-4 pb-12 xl:px-0">
+      <main className="mx-auto flex w-full min-w-[320px] max-w-screen-xl grow flex-col px-2.5 pt-4 pb-12 sm:px-4 xl:px-0">
         <Outlet />
       </main>
-      <footer className="relative mx-auto w-full min-w-[320px] max-w-screen-xl border-t py-12 px-4 dark:border-gray-700 md:py-8 xl:px-0">
-        <div className="flex flex-col items-center justify-around gap-x-4 gap-y-6 md:flex-row">
-          <div className="xts:text-sm flex flex-wrap gap-4 text-xs">
-            <a
-              href="https://github.com/AlexanderOrmseth/MittVinlager"
-              target="_blank"
-              className="flex items-center gap-x-1 rounded border-white bg-gray-900 px-2 py-1 text-white hover:bg-black dark:bg-black/60 dark:hover:bg-black"
-              rel="noreferrer"
-            >
-              <GithubLogo size="1.5em" />
-              GitHub
-            </a>
-            <a
-              href="https://www.linkedin.com/in/alexander-ormseth-731b11239/"
-              target="_blank"
-              className="flex items-center gap-x-1 rounded border-white bg-[#0a66c2] px-2 py-1  text-white hover:bg-[#004182]"
-              rel="noreferrer"
-            >
-              <LinkedinLogo size="1.5em" />
-              LinkedIn
-            </a>
-          </div>
-          <p className="text-sm text-gray-700 dark:text-gray-300">
-            Utviklet av Alexander Ormseth
-          </p>
-        </div>
-        <p className="absolute left-0 -top-2.5 w-full text-center text-sm text-gray-500 dark:text-gray-400">
-          <span className="bg-white px-2 dark:bg-gray-900">
-            Mitt Vinlager © 2021-{new Date().getFullYear()}
-          </span>
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 };
