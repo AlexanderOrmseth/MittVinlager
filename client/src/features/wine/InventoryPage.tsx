c2fc4f3d9bfdd564f9bc06a3b635246372fd9b82
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-17 11:59:00 +0200

    cleanup

--- client/src/features/wine/InventoryPage.tsx
+++ client/src/features/wine/InventoryPage.tsx
@@ -6,7 +6,6 @@ import { useGetAllWineQuery } from "../../app/services/wineApi";
 import Paginator from "./list/Paginator";
 import Spinner from "../../app/components/loading/Spinner";
 import ErrorBox from "../../app/components/ErrorBox";
-import { AnimatePresence } from "framer-motion";
 
 const InventoryPage = () => {
   const params = useAppSelector(getParams);
@@ -17,7 +16,7 @@ const InventoryPage = () => {
   if (isLoading || isFetching) content = <Spinner text="Laster..." />;
   else if (isSuccess)
     content = <WineList wine={data.items} metaData={data.metaData} />;
-  else if (isError) content = <ErrorBox message="Something went wrong.." />;
+  else if (isError) content = <ErrorBox message="Error, noe gikk galt..." />;
 
   return (
     <div className="flex min-h-full flex-1 grow flex-col gap-4 md:flex-row">
@@ -32,11 +31,7 @@ const InventoryPage = () => {
           isLoading={isLoading || isFetching}
           top={true}
         />
-
-        <div className="block-less-muted flex flex-1 flex-col">
-          <AnimatePresence initial={false}>{content}</AnimatePresence>
-        </div>
-
+        <div className="block-less-muted flex flex-1 flex-col">{content}</div>
         <Paginator
           metaData={data?.metaData}
           isLoading={isLoading || isFetching}
